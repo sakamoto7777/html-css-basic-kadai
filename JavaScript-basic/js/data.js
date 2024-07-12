fc345e28fd2d15033fdcbb8fcc7c2b5f931fad6f
sakamoto7777 <ssakamoto7657@gmail.com>
2024-07-13 06:39:18 +0900

    課題013など追加

---
+++ JavaScript-basic/js/data.js
@@ -0,0 +1,28 @@
+// 四則演算
+console.log(45+18);
+console.log(30-12);
+console.log(15*6);
+console.log(30/3);
+console.log(45%8);
+
+// 文字列の連結
+console.log('45' + '18');
+console.log('今日の天気は' + '晴れ');
+console.log('今日の天気は' + '曇り');
+
+ // 変数の宣言
+ let userName;
+
+ // 値の代入
+ userName='侍太郎';
+
+ // 変数の宣言・値の代入
+ let userNumber = 55;
+
+  // コンソールへの出力
+  console.log(userName);
+  console.log(userNumber);
+ 
+ 
+
+
